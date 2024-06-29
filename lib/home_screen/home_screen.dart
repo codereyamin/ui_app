4c584c53e0896550f1937015493010765a798644
Tuhin Ikbal Eyamin <codereyamin@gmail.com>
2024-06-29 23:57:52 +0600

    data show

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -14,121 +14,137 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder(
         init: HomeScreenController(),
-        builder: (context) {
-          return Scaffold(
-            backgroundColor: AppColors.white,
-            body: SafeArea(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: AppSize.height(value: 16.0)),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      TopHeader(),
-                      Container(
-                        height: AppSize.height(value: 32.0),
-                        color: AppColors.secondaryBackground,
+        builder: (controller) {
+          return controller.initialLoad.value
+              ? Scaffold(
+                  body: Center(
+                    child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: CircularProgressIndicator(),
+                    ),
+                  ),
+                )
+              : Scaffold(
+                  backgroundColor: AppColors.white,
+                  body: SafeArea(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: AppSize.height(value: 16.0)),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            TopHeader(),
+                            Container(
+                              height: AppSize.height(value: 32.0),
+                              color: AppColors.secondaryBackground,
+                            ),
+                            ...List.generate(
+                              controller.listOfProduct.length,
+                              (index) {
+                                return HomePageContainer(
+                                  product: controller.listOfProduct[index],
+                                  isLast: controller.listOfProduct.length - 1 == index,
+                                );
+                              },
+                            ),
+                            SizedBox(
+                              height: AppSize.height(value: 12.0),
+                            )
+                          ],
+                        ),
                       ),
-                      HomePageContainer(),
-                      HomePageContainer(),
-                      HomePageContainer(),
-                      HomePageContainer(
-                        isLast: true,
+                    ),
+                  ),
+                  bottomNavigationBar: Container(
+                    padding: EdgeInsets.all(AppSize.height(value: 16.0)),
+                    decoration: BoxDecoration(
+                      color: AppColors.background,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24.0),
+                        topRight: Radius.circular(24.0),
                       ),
-                    ],
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SizedBox(
+                          height: AppSize.height(value: 12.0),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            AppText(
+                              text: "Subtotal",
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              textScaler: TextScaler.linear(0.9),
+                            ),
+                            AppText(
+                              text: "\$310.5",
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              textScaler: TextScaler.linear(0.9),
+                              color: AppColors.primary,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: AppSize.height(value: 14.0),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            AppText(
+                              text: "Vat(5%) Service Charge included (5%)",
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              textScaler: TextScaler.linear(0.9),
+                              color: AppColors.blue,
+                            ),
+                            AppText(
+                              text: "\$5.5",
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              textScaler: TextScaler.linear(0.9),
+                              color: AppColors.blue,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: AppSize.height(value: 16.0),
+                        ),
+                        Container(
+                          height: 2,
+                          color: AppColors.primary,
+                        ),
+                        SizedBox(
+                          height: AppSize.height(value: 8.0),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            AppText(
+                              text: "Total",
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              textScaler: TextScaler.linear(0.9),
+                            ),
+                            AppText(
+                              text: "\$316.0",
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              textScaler: TextScaler.linear(0.9),
+                              color: AppColors.primary,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: AppSize.height(value: 8.0),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ),
-            ),
-            bottomNavigationBar: Container(
-              padding: EdgeInsets.all(AppSize.height(value: 16.0)),
-              decoration: BoxDecoration(
-                color: AppColors.background,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(24.0),
-                  topRight: Radius.circular(24.0),
-                ),
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    height: AppSize.height(value: 12.0),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      AppText(
-                        text: "Subtotal",
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        textScaler: TextScaler.linear(0.9),
-                      ),
-                      AppText(
-                        text: "\$310.5",
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        textScaler: TextScaler.linear(0.9),
-                        color: AppColors.primary,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: AppSize.height(value: 14.0),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      AppText(
-                        text: "Vat(5%) Service Charge included (5%)",
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        textScaler: TextScaler.linear(0.9),
-                        color: AppColors.blue,
-                      ),
-                      AppText(
-                        text: "\$5.5",
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        textScaler: TextScaler.linear(0.9),
-                        color: AppColors.blue,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: AppSize.height(value: 16.0),
-                  ),
-                  Container(
-                    height: 2,
-                    color: AppColors.primary,
-                  ),
-                  SizedBox(
-                    height: AppSize.height(value: 8.0),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      AppText(
-                        text: "Total",
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        textScaler: TextScaler.linear(0.9),
-                      ),
-                      AppText(
-                        text: "\$316.0",
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        textScaler: TextScaler.linear(0.9),
-                        color: AppColors.primary,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: AppSize.height(value: 8.0),
-                  ),
-                ],
-              ),
-            ),
-          );
+                );
         });
   }
 }
--- lib/home_screen/widget/home_page_container.dart
+++ lib/home_screen/widget/home_page_container.dart
@@ -1,5 +1,6 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:ui_app/home_screen/models/product.dart';
 import 'package:ui_app/home_screen/widget/row_border.dart';
 import 'package:ui_app/utils/app_colors.dart';
 import 'package:ui_app/widget/app_circle_image.dart';
@@ -10,8 +11,10 @@ class HomePageContainer extends StatelessWidget {
   const HomePageContainer({
     super.key,
     this.isLast = false,
+    required this.product,
   });
   final bool isLast;
+  final Product product;
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +37,8 @@ class HomePageContainer extends StatelessWidget {
                     decoration: BoxDecoration(color: AppColors.background, borderRadius: BorderRadius.circular(12.0)),
                     child: AppCircleImage(
                       size: 40,
+                      url: product.image,
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
@@ -47,7 +52,7 @@ class HomePageContainer extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         AppText(
-                          text: "Rounded Table Piza",
+                          text: product.name ?? "",
                           fontWeight: FontWeight.bold,
                           fontSize: 16,
                         ),
@@ -55,7 +60,7 @@ class HomePageContainer extends StatelessWidget {
                           height: AppSize.height(value: 8.0),
                         ),
                         AppText(
-                          text: "Rounded Table Piza",
+                          text: product.description ?? "",
                         ),
                       ],
                     )),
@@ -65,7 +70,7 @@ class HomePageContainer extends StatelessWidget {
                 Column(
                   children: [
                     AppText(
-                      text: "\$310.5",
+                      text: product.discountPrice == null || product.discountPrice == 0 ? "\$${product.price}" : "\$${product.discountPrice}",
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
                       textScaler: TextScaler.linear(0.9),
@@ -74,30 +79,7 @@ class HomePageContainer extends StatelessWidget {
                     SizedBox(
                       height: AppSize.height(value: 12.0),
                     ),
-                    // Container(
-                    //   padding: EdgeInsets.symmetric(horizontal: AppSize.width(value: 16), vertical: AppSize.height(value: 4.5)),
-                    //   decoration: BoxDecoration(border: Border.all(color: AppColors.primary)),
-                    //   child: AppText(
-                    //     text: "Add",
-                    //     color: AppColors.primary,
-                    //     fontWeight: FontWeight.bold,
-                    //   ),
-                    // ),
-
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: AppSize.width(value: 4.0), vertical: AppSize.height(value: 4.5)),
-                      decoration: BoxDecoration(border: Border.all(color: AppColors.primary)),
-                      child: Row(
-                        children: [
-                          Icon(Icons.remove),
-                          AppText(
-                            text: "25",
-                            fontWeight: FontWeight.bold,
-                          ),
-                          Icon(Icons.add)
-                        ],
-                      ),
-                    )
+                    getWidget(product.isClick ?? false),
                   ],
                 ),
               ],
@@ -115,4 +97,31 @@ class HomePageContainer extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget getWidget(bool value) {
+  return value
+      ? Container(
+          padding: EdgeInsets.symmetric(horizontal: AppSize.width(value: 4.0), vertical: AppSize.height(value: 4.5)),
+          decoration: BoxDecoration(border: Border.all(color: AppColors.primary)),
+          child: Row(
+            children: [
+              Icon(Icons.remove),
+              AppText(
+                text: "25",
+                fontWeight: FontWeight.bold,
+              ),
+              Icon(Icons.add)
+            ],
+          ),
+        )
+      : Container(
+          padding: EdgeInsets.symmetric(horizontal: AppSize.width(value: 16), vertical: AppSize.height(value: 4.5)),
+          decoration: BoxDecoration(border: Border.all(color: AppColors.primary)),
+          child: AppText(
+            text: "Add",
+            color: AppColors.primary,
+            fontWeight: FontWeight.bold,
+          ),
+        );
 }
